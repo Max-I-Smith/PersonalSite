532c96786e59b1afd075dac41cb308ed9755f54d
Max-I-Smith <maxsmith3169@gmail.com>
2022-01-04 17:08:40 -0600

    Resume Added, Contact Page not working

--- PersonalSite/PersonalSiteMVC/Controllers/ContactController.cs
+++ PersonalSite/PersonalSiteMVC/Controllers/ContactController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using PersonalSiteMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +13,45 @@ namespace PersonalSiteMVC.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
-        public ActionResult Index()
+        public ActionResult Contact()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = $"{cvm.Name} has sent you the following message:<br/>" +
+                    $"{cvm.Message}<br/> from the following email:<br/>{cvm.Email}";
+
+                MailMessage mm = new MailMessage(
+                    ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                    ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                    "",
+                    body);
+                mm.IsBodyHtml = true;
+
+                SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+                client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch(Exception ex)
+                {
+                    ViewBag.CustomMessage =
+                        $"I'm sorry your request could not be completed at this time." +
+                        $"  Please try again later. Error Message: <br/> {ex.StackTrace}";
+                    return View(cvm);
+                }
+                return View("EmailConfirmation", cvm);
+                }
+            return View(cvm);
         }
     }
 }
---
+++ PersonalSite/PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="Your name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Your email address is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "A Message is required.")]
+        public string Message { get; set; }
+    }
+}
